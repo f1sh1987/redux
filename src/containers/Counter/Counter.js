89075fc6f8bda3bc0fa6b996c0fad4519f2c2b2b
afischer <andreas.fischer@prodyna.com>
2018-09-26 13:52:51 +0200

    redux fertig

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -20,7 +20,7 @@ class Counter extends Component {
                 <CounterControl label="Add 10" clicked={this.props.onADDIncrementCounter}  />
                 <CounterControl label="Subtract 15" clicked={this.props.onSubstractCounter}  />
                 <hr />
-                <button onClick={() =>this.props.onStoreResult(this.props.ctr)}>Store Result</button>
+                <button onClick={() =>this.props.onStoreResult}>Store Result</button>
                 <ul>
                     {this.props.storedResults.map(item => ( 
                     <li key={item.id} onClick={() =>this.props.onDeleteResult(item.id)}>{item.value}:{item.id.toString()}</li>) )}
